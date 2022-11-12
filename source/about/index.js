bb01dcbb4d625f61d4c5441502cc8c525a2b3954
Rcong <zcr0903@gmail.com>
2022-11-12 10:48:07 +0800

    beautify the style

--- source/about/index.js
+++ source/about/index.js
@@ -5,7 +5,7 @@
       contact: {
         title: 'Contact',
         list: [
-          { label: 'Address', value: 'Glenfield, AUK, 6029' },
+          { label: 'Address', value: 'Glenfield, AUK, 6029</br>(Valid open work visa and willing to relocate)' },
           { label: 'Phone', value: '027-386-8521' },
           { label: 'E-mail', value: 'yanqizhao.career@gmail.com', link: 'mailto:' },
           { label: 'LinkedIn', value: 'linkedin/in/yanqizhao', link: 'https://' },
